3d9acd176b36ba87c00f9ffa773140aa89841d6b
finn753 <itsfinn886@gmail.com>
2024-03-21 17:29:42 +0100

    More responsiveness tweaks

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,6 +1,6 @@
 
-<div class="h-dvh w-dvw flex flex-col-reverse sm:flex-row">
-	<div class="h-16 w-dvw sm:w-16 sm:h-dvh bg-background shadow-lg">
+<div class="h-dvh w-dvw flex flex-col-reverse md:flex-row">
+	<div class="h-16 w-dvw md:w-16 md:h-dvh bg-background shadow-lg">
 
 	</div>
 	<div class="flex-1">
--- src/routes/(app)/chats/+layout.svelte
+++ src/routes/(app)/chats/+layout.svelte
@@ -1,10 +1,10 @@
-<div class="grid grid-cols-5 h-full">
+<div class="grid grid-cols-4 lg:grid-cols-5 h-full">
 	<div class="col-span-1">
 		<div class="bg-level-two shadow-lg size-full">
 
 		</div>
 	</div>
-	<div class="col-span-3">
+	<div class="col-span-2 lg:col-span-3">
 		<slot/>
 	</div>
 	<div class="col-span-1">
