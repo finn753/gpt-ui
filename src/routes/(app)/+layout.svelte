986308d2617c203d4b1fe4d8f68b1f2241e3ec6f
finn753 <itsfinn886@gmail.com>
2024-03-21 17:24:10 +0100

    Responsive Sidebar

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,6 +1,6 @@
 
-<div class="flex">
-	<div class="w-12 h-dvh bg-background">
+<div class="h-dvh w-dvw flex flex-col-reverse sm:flex-row">
+	<div class="h-16 w-dvw sm:w-16 sm:h-dvh bg-background shadow-lg">
 
 	</div>
 	<div class="flex-1">
--- src/routes/(app)/chats/+layout.svelte
+++ src/routes/(app)/chats/+layout.svelte
@@ -1,11 +1,15 @@
-<div class="grid grid-cols-5 h-dvh">
-	<div class="bg-level-two">
+<div class="grid grid-cols-5 h-full">
+	<div class="col-span-1">
+		<div class="bg-level-two shadow-lg size-full">
 
+		</div>
 	</div>
 	<div class="col-span-3">
 		<slot/>
 	</div>
-	<div class="bg-level-two">
+	<div class="col-span-1">
+		<div class="bg-level-two shadow-lg size-full">
 
+		</div>
 	</div>
 </div>
