0a6e50a0afa0a2c84c5927dbb35413f7da77e354
finn753 <itsfinn886@gmail.com>
2024-03-22 23:37:59 +0100

    Sidebar icon style changes
    Thicker line and darker color

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -3,7 +3,8 @@
 	import { MessagesSquare, User, Images, Brush, LayoutTemplate } from "lucide-svelte";
 
 	const size = 24;
-	const strokeWidth = 1;
+	const strokeWidth = 2;
+	const color = "#64748B"
 </script>
 
 <div class="flex h-dvh w-dvw flex-col-reverse md:flex-row">
@@ -11,25 +12,25 @@
 		class="z-20 flex h-16 w-dvw items-center justify-evenly bg-background p-4 shadow-lg md:h-dvh md:w-16 md:flex-col md:space-y-4"
 	>
 		<Button variant="ghost" href="/chats">
-			<MessagesSquare {size} {strokeWidth} />
+			<MessagesSquare {size} {strokeWidth} {color} />
 		</Button>
 
 		<Button variant="ghost" href="/generator">
-			<Brush {size} {strokeWidth} />
+			<Brush {size} {strokeWidth} {color} />
 		</Button>
 
 		<Button variant="ghost" href="/gallery">
-			<Images {size} {strokeWidth} />
+			<Images {size} {strokeWidth} {color} />
 		</Button>
 
 		<Button variant="ghost" href="/templates">
-			<LayoutTemplate {size} {strokeWidth} />
+			<LayoutTemplate {size} {strokeWidth} {color} />
 		</Button>
 
 		<div class="hidden md:block md:flex-1"></div>
 
 		<Button variant="ghost" href="/settings">
-			<User {size} {strokeWidth} />
+			<User {size} {strokeWidth} {color} />
 		</Button>
 	</div>
 	<div class="flex-1">
