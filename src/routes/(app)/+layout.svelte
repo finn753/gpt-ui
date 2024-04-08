316fb67afa41f764c6799487e986b19893ed79e8
finn753 <itsfinn886@gmail.com>
2024-04-08 12:58:06 +0200

    Added tooltips for navigation buttons

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -6,6 +6,7 @@
 	import { onMount } from "svelte";
 
 	import { page } from '$app/stores';
+	import * as Tooltip from "$lib/components/ui/tooltip";
 
 	$: currentPath = $page.url.pathname;
 
@@ -32,27 +33,63 @@
 	<div
 		class="z-20 flex h-16 w-dvw items-center justify-evenly bg-background p-4 shadow-lg md:h-dvh md:w-16 md:flex-col md:space-y-4"
 	>
-		<Button variant={currentPath.startsWith("/chats") ? "secondary" : "ghost"} href="/chats">
-			<MessagesSquare {size} {strokeWidth} {color} />
-		</Button>
+		<Tooltip.Root>
+			<Tooltip.Trigger>
+				<Button variant={currentPath.startsWith("/chats") ? "secondary" : "ghost"} href="/chats">
+					<MessagesSquare {size} {strokeWidth} {color} />
+				</Button>
+			</Tooltip.Trigger>
+			<Tooltip.Content>
+				Chats
+			</Tooltip.Content>
+		</Tooltip.Root>
 
-		<Button variant={currentPath.startsWith("/generator") ? "secondary" : "ghost"} href="/generator">
-			<Brush {size} {strokeWidth} {color} />
-		</Button>
 
-		<Button variant={currentPath.startsWith("/gallery") ? "secondary" : "ghost"} href="/gallery">
-			<Images {size} {strokeWidth} {color} />
-		</Button>
+		<Tooltip.Root>
+			<Tooltip.Trigger>
+				<Button variant={currentPath.startsWith("/generator") ? "secondary" : "ghost"} href="/generator">
+					<Brush {size} {strokeWidth} {color} />
+				</Button>
+			</Tooltip.Trigger>
+			<Tooltip.Content>
+				Generator
+			</Tooltip.Content>
+		</Tooltip.Root>
 
-		<Button variant={currentPath.startsWith("/templates") ? "secondary" : "ghost"} href="/templates">
-			<LayoutTemplate {size} {strokeWidth} {color} />
-		</Button>
+		<Tooltip.Root>
+			<Tooltip.Trigger>
+				<Button variant={currentPath.startsWith("/gallery") ? "secondary" : "ghost"} href="/gallery">
+					<Images {size} {strokeWidth} {color} />
+				</Button>
+			</Tooltip.Trigger>
+			<Tooltip.Content>
+				Gallery
+			</Tooltip.Content>
+		</Tooltip.Root>
+
+		<Tooltip.Root>
+			<Tooltip.Trigger>
+				<Button variant={currentPath.startsWith("/templates") ? "secondary" : "ghost"} href="/templates">
+					<LayoutTemplate {size} {strokeWidth} {color} />
+				</Button>
+			</Tooltip.Trigger>
+			<Tooltip.Content>
+				Templates
+			</Tooltip.Content>
+		</Tooltip.Root>
 
 		<div class="hidden md:block md:flex-1"></div>
 
-		<Button variant={currentPath.startsWith("/settings") ? "secondary" : "ghost"} href="/settings">
-			<User {size} {strokeWidth} {color} />
-		</Button>
+		<Tooltip.Root>
+			<Tooltip.Trigger>
+				<Button variant={currentPath.startsWith("/settings") ? "secondary" : "ghost"} href="/settings">
+					<User {size} {strokeWidth} {color} />
+				</Button>
+			</Tooltip.Trigger>
+			<Tooltip.Content>
+				Settings
+			</Tooltip.Content>
+		</Tooltip.Root>
 	</div>
 	<div class="h-[calc(100dvh-4rem)] flex-1 md:h-dvh">
 		<slot />
