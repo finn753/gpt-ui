9a8d6fc47c14aa20a718cfd057140fbc01f39af7
finn753 <itsfinn886@gmail.com>
2024-04-08 12:53:44 +0200

    Highlight navigation buttons

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -5,6 +5,10 @@
 	import { initializeModel } from "client-vector-search";
 	import { onMount } from "svelte";
 
+	import { page } from '$app/stores';
+
+	$: currentPath = $page.url.pathname;
+
 	const size = 24;
 	const strokeWidth = 2;
 	const color = "#64748B";
@@ -28,25 +32,25 @@
 	<div
 		class="z-20 flex h-16 w-dvw items-center justify-evenly bg-background p-4 shadow-lg md:h-dvh md:w-16 md:flex-col md:space-y-4"
 	>
-		<Button variant="ghost" href="/chats">
+		<Button variant={currentPath.startsWith("/chats") ? "secondary" : "ghost"} href="/chats">
 			<MessagesSquare {size} {strokeWidth} {color} />
 		</Button>
 
-		<Button variant="ghost" href="/generator">
+		<Button variant={currentPath.startsWith("/generator") ? "secondary" : "ghost"} href="/generator">
 			<Brush {size} {strokeWidth} {color} />
 		</Button>
 
-		<Button variant="ghost" href="/gallery">
+		<Button variant={currentPath.startsWith("/gallery") ? "secondary" : "ghost"} href="/gallery">
 			<Images {size} {strokeWidth} {color} />
 		</Button>
 
-		<Button variant="ghost" href="/templates">
+		<Button variant={currentPath.startsWith("/templates") ? "secondary" : "ghost"} href="/templates">
 			<LayoutTemplate {size} {strokeWidth} {color} />
 		</Button>
 
 		<div class="hidden md:block md:flex-1"></div>
 
-		<Button variant="ghost" href="/settings">
+		<Button variant={currentPath.startsWith("/settings") ? "secondary" : "ghost"} href="/settings">
 			<User {size} {strokeWidth} {color} />
 		</Button>
 	</div>
