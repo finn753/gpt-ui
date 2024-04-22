18edd4738b8d436bb3c4cc96ac161212553be969
finn753 <itsfinn886@gmail.com>
2024-04-22 10:59:37 +0200

    Change Password

---
+++ src/routes/(app)/settings/account/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import ChangePasswordSettings from "$lib/components/settings/ChangePasswordSettings.svelte";
+</script>
+
+<div class="flex flex-col gap-2">
+	<h1 class="text-2xl font-bold">Integrations</h1>
+
+	<ChangePasswordSettings />
+</div>
