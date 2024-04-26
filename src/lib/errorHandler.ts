7b4a5d4c7852fcde21744e2bb2e7207b9baffd20
finn753 <itsfinn886@gmail.com>
2024-04-26 22:01:52 +0200

    More detailed error messages

--- src/lib/errorHandler.ts
+++ src/lib/errorHandler.ts
@@ -2,5 +2,5 @@ import { toast } from "svelte-sonner";
 
 export function handleError(message: string, error: unknown) {
 	console.error(message, error);
-	toast.error(message);
+	toast.error(message + ": " + error);
 }
