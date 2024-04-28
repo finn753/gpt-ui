c17ddb78d1f93a6899ee92e2fb16d2e5825108de
finn753 <itsfinn886@gmail.com>
2024-04-28 13:36:48 +0200

    getAvailableModels error handling

--- src/lib/modelManager.ts
+++ src/lib/modelManager.ts
@@ -16,25 +16,35 @@ export async function getAvailableModels(): Promise<ModelType[]> {
 }
 
 export async function getAvailableOpenAIModels(): Promise<ModelType[]> {
-	const openAI = new OpenAI({ apiKey: get(openaiApiKey) ?? "" });
-	return (await openAI.models.list()).data
-		.filter((model) => /ft:|gpt-/.test(model.id))
-		.filter((model) => /(gpt-3.5-turbo|gpt-4-turbo)$/.test(model.id))
-		.map((model) => ({
-			id: "openai:" + model.id,
-			name: model.id,
-			provider: "openai",
-			vision: model.id.includes("vision") || model.id === "gpt-4-turbo"
-		}));
+	try {
+		const openAI = new OpenAI({ apiKey: get(openaiApiKey) ?? "" });
+		return (await openAI.models.list()).data
+			.filter((model) => /ft:|gpt-/.test(model.id))
+			.filter((model) => /(gpt-3.5-turbo|gpt-4-turbo)$/.test(model.id))
+			.map((model) => ({
+				id: "openai:" + model.id,
+				name: model.id,
+				provider: "openai",
+				vision: model.id.includes("vision") || model.id === "gpt-4-turbo"
+			}));
+	} catch (e: unknown) {
+		console.error(e);
+		return [];
+	}
 }
 
 export async function getAvailableOllamaModels(): Promise<ModelType[]> {
-	return (await ollama.list()).models.map((model) => ({
-		id: "ollama:" + model.name,
-		name: model.name,
-		provider: "ollama",
-		vision: model.details.families.includes("clip")
-	}));
+	try {
+		return (await ollama.list()).models.map((model) => ({
+			id: "ollama:" + model.name,
+			name: model.name,
+			provider: "ollama",
+			vision: model.details.families.includes("clip")
+		}));
+	} catch (e: unknown) {
+		console.error(e);
+		return [];
+	}
 }
 
 export function getLangchainModelById(
