ec60466c8036aa60751bc0289e18512a78b95d88
finn753 <itsfinn886@gmail.com>
2024-05-25 23:26:13 +0200

    Better ContentEditor Navigation

--- src/lib/components/workflows/ContentEditor.svelte
+++ src/lib/components/workflows/ContentEditor.svelte
@@ -1,34 +1,100 @@
 <script lang="ts">
 	// @ts-expect-error - svelte-autosize doesn't have types
 	import autosize from "svelte-autosize";
-	import { onMount, afterUpdate } from "svelte";
+	import { onMount, afterUpdate, tick } from 'svelte';
 	import { cn } from "$lib/utils";
 
 	let valueSegments: string[] = [];
+	let textareaElements: HTMLTextAreaElement[] = [];
 	export let value = "";
 	export let disabled = false;
 	export let placeholder = "";
 
 	onMount(() => (valueSegments = value.split("\n\n")));
-	$: valueSegments = value.split("\n\n");
+	$: valueSegments = reverseSplit(value, "\n\n");
 
 	afterUpdate(() => (value = valueSegments.join("\n\n")));
 
-	function handleKeyDown(i: number, event: KeyboardEvent) {
+	function reverseSplit(str: string, separator: string) {
+		return str.split('').reverse().join('').split(separator).map(item => item.split('').reverse().join('')).reverse();
+	}
+
+	async function handleKeyDown(i: number, event: KeyboardEvent) {
 		const target = event.target as HTMLTextAreaElement;
 
 		if (
 			event.key === "Backspace" &&
 			target.selectionStart === 0 &&
-			target.selectionEnd === 0 &&
-			i > 0
+			target.selectionEnd === 0
 		) {
 			event.preventDefault();
+			handleBackspace(i);
+		}
+
+		if (event.key === "Enter") {
+			event.preventDefault();
+
+			await handleEnter(i, event);
+		}
+
+		if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.key)) {
+			handleArrowKeys(i, event)
+		}
+	}
+
+	async function handleEnter(i: number, event: KeyboardEvent) {
+		const target = event.target as HTMLTextAreaElement;
+
+		if(event.shiftKey) {
+			const cursorPosition = target.selectionStart;
+			const lineStart = target.value.lastIndexOf("\n", cursorPosition - 1) + 1;
+			let lineEnd = target.value.indexOf("\n", cursorPosition);
+			if (lineEnd === -1) lineEnd = target.value.length;
+
+			const currentLine = target.value.substring(lineStart, lineEnd);
+
+			if(currentLine === "") return;
+
+			if(target.value[cursorPosition] === "\n") return;
+
+			valueSegments[i] = target.value.substring(0, cursorPosition) + "\n" + target.value.substring(cursorPosition);
+		} else {
+			if(target.value === "") return;
+
+			valueSegments = valueSegments.toSpliced(i + 1, 0, "");
+			await tick();
+			textareaElements[i + 1].focus();
+		}
+	}
+
+	function handleBackspace(i: number) {
+		if(i > 0) {
 			if (valueSegments[i] === "") {
 				valueSegments = valueSegments.toSpliced(i, 1);
 			} else {
 				valueSegments[i - 1] += "\n" + valueSegments[i];
 				valueSegments.splice(i, 1);
+			}
+			textareaElements[i - 1].focus();
+		} else {
+			if(valueSegments.length > 1) {
+				valueSegments = valueSegments.toSpliced(i, 1);
+			}
+		}
+	}
+
+	function handleArrowKeys(i: number, event: KeyboardEvent) {
+		const target = event.target as HTMLTextAreaElement;
+
+		if (event.key === "ArrowUp" && target.selectionStart === 0) {
+			if (i > 0) {
+				textareaElements[i - 1].focus();
+			}
+		}
+
+		if (event.key === "ArrowDown" && target.selectionStart === target.value.length) {
+			if (i < textareaElements.length - 1) {
+				textareaElements[i + 1].focus();
 			}
 		}
 	}
@@ -55,6 +121,7 @@
 					getStyleClassesForValue(segment)
 				)}
 				bind:value={segment}
+				bind:this={textareaElements[i]}
 				use:autosize
 				{disabled}
 				{placeholder}
